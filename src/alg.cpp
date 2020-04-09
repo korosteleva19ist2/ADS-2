2001aa16ed31f5ef51856259a6394991cae46dfb
korosteleva19ist2 <62651383+korosteleva19ist2@users.noreply.github.com>
2020-04-09 19:59:53 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+int countPairs1(int* arr, int len, int value)
+{
+	int count = 0;
+	for (int i = 0; i < len-1; i++)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[i] + arr[j] == value)
+				count++;
+		}
+	}
+	return count;
+}
+int countPairs2(int* arr, int len, int value)
+{
+	int count = 0;
+	for (int i = 0; i <= len/2; i++)
+	{
+		for (int j = len; j > len/2; j--)
+		{
+			if (arr[i] + arr[j] == value)
+				count++;
+		}
+	}
+	return count;
+}
+int countPairs3(int* arr, int len, int value)
+{
+	int count = 0;
+	int mid = 0;
+	for (int i = 0; i <= len; i++)
+	{
+		int lx = len, rx = len;
+		int left = i + 1, right = len - 1;
+		bool k = 0;
+		while (left <= right) 
+		{
+			mid = (left + right) / 2;
+			if (arr[mid] == value - arr[i]) 
+				k = 1;
+			if (arr[mid] > value - arr[i]) 
+			{
+				lx = mid;
+				right = mid - 1;
+			}
+			else
+			{
+				left = mid + 1;
+			}
+		}
+		left = i + 1; right = len - 1;
+		while (left <= right) 
+		{
+			mid = (left + right) / 2;
+			if (arr[mid] == value - arr[i]) 
+				k = 1;
+			if (arr[mid] >= value - arr[i]) 
+			{
+				rx = mid;
+				right = mid - 1;
+			}
+			else
+				left = mid + 1;
+		}
+		if (k) 
+			count += (lx - rx);
+	}
+	return count;
+}
+int main()
+{
+	int arr[] = {2,3,3,4,5,5,6,7,8};
+	cout << countPairs1(arr, 9, 10) << endl;
+	cout << countPairs2(arr, 9, 10) << endl;
+	cout << countPairs3(arr, 9, 10) << endl;
+}
